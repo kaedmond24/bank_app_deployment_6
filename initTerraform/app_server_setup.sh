86c1903b2f78fa5a664b6f37a027e75008c569aa
Kedmond24 <Kedmond24@live.com>
2023-10-30 17:36:22 -0400

    UPDATE script formatting to one package per line

--- initTerraform/app_server_setup.sh
+++ initTerraform/app_server_setup.sh
@@ -2,10 +2,13 @@
 
 # Install Required Packages
 apt-get update
-apt-get install software-properties-common
+apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
-apt-get install -y python3.7, python3.7-venv, python3.7-dev
-apt-get install -y build-essential, libmysqlclient-dev
+apt-get install -y python3.7
+apt-get install -y python3.7-venv
+apt-get install -y build-essential
+apt-get install -y libmysqlclient-dev
+apt-get install -y python3.7-dev
 
 # Create Virtual Env and Run Application
 python3.7 -m venv test
@@ -18,9 +21,5 @@ pip install -r requirements.txt
 pip install gunicorn
 pip install mysqlclient
 
-# Run Application
-# python database.py
-# sleep 1
-# python load_data.py
-# sleep 1 
+# Start Application
 python -m gunicorn app:app -b 0.0.0.0 -D
